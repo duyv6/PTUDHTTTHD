ce6f1d8ffb0490b91fb2813df8f2e07039ac6a9e
Luật <caibanghoi9@gmail.com>
2017-05-26 15:51:25 +0700

    giao diện trang login

---
+++ Code/BankWebsite/js/JavaScript.js
@@ -0,0 +1,26 @@
+﻿function validation() {
+    var userName = document.getElementsByName("username")[0].value;
+    var passWord = document.getElementsByName("password")[0].value;
+    if(userName!=''&&passWord!='')
+    {
+        LoadKhachHang();
+    }
+}
+
+function LoadKhachHang() {
+    _url = 'http://localhost:54153/api/nhanvien/hello';
+    $.ajax({
+        url: _url,
+        dataType: 'json',
+        timeout: 30 * 1000
+    })
+    .done(function (data) {
+        $.each(data, function (index, dm) {
+            console.log(dm.MaNhanVien)
+        });
+    })
+    .fail(function (jqXHR, textStatus, error) {
+        console.log(jqXHR);
+        console.log(jqXHR.statusCode);
+    });
+}
